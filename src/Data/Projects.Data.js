66e58b76ade308fe39a5486744adc28b3a5a58b0
rachit gour <rachit.gour@grid-os.com>
2025-06-04 01:09:04 +0530

    updated

--- src/Data/Projects.Data.js
+++ src/Data/Projects.Data.js
@@ -5,8 +5,8 @@ import YumRun from '../assets/yumrun.png'
 export const projects = [
     {
       title: "YumRun (Food Delivery Web App)",
-      description: "Full-stack e-commerce solution with React, Node.js, and MongoDB. Features include user authentication, payment integration, and admin dashboard.",
-      tech: ["React", "Node.js", "MongoDB", "Stripe", "Redux"],
+      description: "YumRun is a full-featured food delivery web app designed to make ordering meals fast, convenient, and enjoyable. Users can browse restaurant menus, customize their orders, and receive real-time updates from checkout to doorstep.",
+      tech: ["React", "Node.js", "MongoDB", "Stripe", "Contex API"],
       image: YumRun,
       github: "https://github.com/Rachit-SDE/YumRun.git",
       live: "https://yumrun-z9ry.onrender.com/",
